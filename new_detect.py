250a32eb84e7c4e76a1b5f5d97f5f0d57282de78
Neftali <neftalibarrera03@gmai.com>
2023-05-04 08:03:16 -0600

    Meti la pagina a la carpeta visiomex

--- new_detect.py
+++ new_detect.py
@@ -1,3 +1,12 @@
+import torch
+import cv2
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/neftali/Escritorio/programas/4to_semestre/pi4/best.pt')
+
+
+model.conf = 0.3  # Umbral de confianza
+model.iou = 0.3 # Umbral de IOU
+
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
