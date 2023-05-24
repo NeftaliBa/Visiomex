3774f23cdc1430c992127444ea8f06f0993c5c41
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2023-05-24 17:07:28 -0600

    Delete visiomex php directory

--- visiomex php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// conectare a base de datos
-$conn = new mysqli("localhost", "root", "Admin123?", "visiomex");
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
