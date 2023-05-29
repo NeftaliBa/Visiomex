8e48c53b56b300f252b5f3c76dc18aa2d6a7c007
Neftali <neftalibarrera03@gmai.com>
2023-05-28 19:58:23 -0600

    Sobre nosotros

--- Sitio_web/views/equipo.php
+++ Sitio_web/views/equipo.php
@@ -24,79 +24,80 @@
                         </ul>
                 </nav>
         </header>
-
-        <header class="header">
-        <div class="container-head logo-nav-container">
-            <a href="#" class="logo">DREAMBUCKS</a>
-            <div class="menu-icon">Menú</div>
-            <nav class="navigation">
-                <ul>
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="Login_U.php">Usuario</a></li>
-                    <li><a href="Login_A.php">Ejecutivo</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-     
     <main>
 
-        <div class="container-background-triangle">
-            <div class="triangle triangle1"> </div>
-            <div class="triangle triangle2"> </div>
-            <div class="triangle triangle3"> </div>
-        </div>
 
         <div class="container-cards">
-            <div class="card">
+        <div class="card">
                 <div class="cover-card">
-                    <img src="./images/hector.jpeg" alt="">
+                    <img src="/resources/img/person.jpeg" alt="">
                 </div>
-                <h2>Héctor Huerta</h2>
+                <h2>Angel Ernesto Jimenez Hernandez</h2>
                 <p>Ejecutivo dedicado con 5 años de experiencia y atención al cliente excelente.</p>
                 <hr>
                 <div class="footer-card">
                     <h3 class="extra-info">En dreambucks desde</h3>
-                    <i>10 de noviembre del 2017</i>
+
+                </div>
+            </div>
+            
+            <div class="card">
+                <div class="cover-card">
+                    <img src="/resources/img/person.jpeg" alt="">
+                </div>
+                <h2>Geremi Josue Gomez Chilapa</h2>
+                <p>Ejecutivo dedicado con 5 años de experiencia y atención al cliente excelente.</p>
+                <hr>
+                <div class="footer-card">
+                    <h3 class="extra-info">En dreambucks desde</h3>
                 </div>
             </div>
 
             <div class="card">
                 <div class="cover-card">
-                    <img src="./images/adair.jpg" alt="">
+                    <img src="/resources/img/person.jpeg" alt="">
                 </div>
-                <h2>Adair Fernandez</h2>
+                <h2>Matias Tirado Morquecho</h2>
                 <p>Administrador, filantropo, modelo, ejecutivo de Dreambucks el cual lleva un reconocido historial de apoyo a sus clientes.</p>
                 <hr>
                 <div class="footer-card">
                     <h3 class="extra-info">En dreambucks desde</h3>
-                    <i>20 de abril del 2015</i>
                 </div>
             </div>
 
             <div class="card">
                 <div class="cover-card">
-                    <img src="./images/neftali.jpg" alt="">
+                    <img src="/resources/img/person.jpeg" alt="">
                 </div>
-                <h2>Neftali Barrera</h2>
+                <h2>Mauricio Chavez Mancilla</h2>
+                <p>Administrador, filantropo, modelo, ejecutivo de Dreambucks el cual lleva un reconocido historial de apoyo a sus clientes.</p>
+                <hr>
+                <div class="footer-card">
+                    <h3 class="extra-info">En dreambucks desde</h3>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="cover-card">
+                    <img src="/resources/img/person.jpeg" alt="">
+                </div>
+                <h2>Yahir Alejandro Estrada Santiago</h2>
                 <p>Ejecutivo joven, entusiasta el cual brinda de uns servicio al cliente acogedor y siempre con la iniciativa de resolver problemas con cuentas.</p>
                 <hr>
                 <div class="footer-card">
                     <h3 class="extra-info">En dreambucks desde</h3>
-                    <i>2 de agosto del 2020</i>
                 </div>
             </div>
 
             <div class="card">
                 <div class="cover-card">
-                    <img src="./images/matias.jpg" alt="">
+                    <img src="/resources/img/person.jpeg" alt="">
                 </div>
-                <h2>Martias Morquecho</h2>
+                <h2>Neftali Barrera Rodriguez</h2>
                 <p>Ejecutivo con alto conocimiento en finanzas, es muy dificl interceptar errores por parte de este gran ejecutivo.</p>
                 <hr>
                 <div class="footer-card">
                     <h3 class="extra-info">En dreambucks desde</h3>
-                    <i>3 de enero del 2016</i>
                 </div>
             </div>
         </div>
